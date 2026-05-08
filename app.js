12e8407d5a748d7dd2351310adf45e2e5c576a2c
genius-el <elvismichael888@gmail.com>
2026-05-08 07:53:30 +0100

    feat: Add recent transactions functionality

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ let expenseAmount = document.getElementById('expense-amount');
 let budgetDate = document.getElementById('budget-date');
 let budgetAmount = document.getElementById('budget-amount');
 
+// Transactions Table references
+let tableBody = document.getElementById('tableBody');
+
 // overlay
 let overlay = document.querySelector('.overlay');
 
@@ -46,9 +49,9 @@ let submitExpenseBtn = document.getElementById('submitExpenseBtn');
 let submitBudgetBtn = document.getElementById('submitBudgetBtn');
 
 // State Values
-let earningValue = 0;
-let expenseValue = 0;
-let budgetValue = 0;
+let totalEarningValue = 0;
+let totalExpenseValue = 0;
+let fixedBudgetValue = 0;
 
 // Event Listeners
 // Opening Modals
@@ -119,6 +122,7 @@ let expenseTransaction = {
     type: "",
 }
 
+let budgetTransactions = [];
 let budgetTransaction = {
     id: "",
     date: "",
@@ -141,7 +145,53 @@ submitEarningBtn.addEventListener('click', function () {
         type: 'Earning'        
     }
 
-    earningValue += Math.round((actualEarningAmount + Number.EPSILON) * 100) / 100;
+    // Complicated method to round to 2 decimal places without floating point issues, but it works!
+    // totalEarningValue += Math.round((Number(earningTransaction.amount) + Number.EPSILON) * 100) / 100;
+    totalEarningValue += parseFloat(earningTransaction.amount);
+    // Displaying the current earning value on the card summary
+    currentEarningValue.textContent = `$${totalEarningValue.toFixed(2)}`;
+    
+    // Populating the earning transactions array
+    earningTransactions.push(earningTransaction);
+
+    // DYNAMICALLY CREATING AND ADDING EARNING TRANSACTION ROWS TO THE TRANSACTIONS TABLE
+    // Create the row element
+    const newEarningRow = document.createElement('tr');
+
+    // Create the date cell
+    const earningDateCell = document.createElement('td');
+    earningDateCell.textContent = earningTransaction.date;
+
+    // Create the amount cell
+    const earningAmountCell = document.createElement('td');
+    earningAmountCell.textContent = `$${earningTransaction.amount}`;
+
+    // Create the description cell
+    const earningDescriptionCell = document.createElement('td');
+    earningDescriptionCell.textContent = earningTransaction.description;
+
+    // Create type cell
+    const earningTypeCell = document.createElement('td');
+    earningTypeCell.textContent = earningTransaction.type;
+
+    // Append each earning cell to the earning row
+    newEarningRow.appendChild(earningDateCell);
+    newEarningRow.appendChild(earningDescriptionCell);
+    newEarningRow.appendChild(earningAmountCell);
+    newEarningRow.appendChild(earningTypeCell);
+
+    // Append the completed earning row to the table body
+    tableBody.appendChild(newEarningRow);
+
+
+    // Clearing all entries
+    earningDate.value = "";
+    earningAmount.value = "";
+    earningDescription.value = "";
+
+    
+
+    closeAllModals();
 })
 
 submitExpenseBtn.addEventListener('click', function () {
@@ -157,7 +207,51 @@ submitExpenseBtn.addEventListener('click', function () {
         type: 'Expense'     
     }
 
-    expenseValue += Math.round((actualExpenseAmount + Number.EPSILON) * 100) / 100;
+    // Complicated method to round to 2 decimal places without floating point issues, but it works!
+    // totalExpenseValue += Math.round((Number(expenseTransaction.amount) + Number.EPSILON) * 100) / 100;
+    totalExpenseValue += parseFloat(expenseTransaction.amount);
+    // Displaying the current expense value on the card summary
+    currentExpenseValue.textContent = `$${totalExpenseValue.toFixed(2)}`;
+    // Populating the expense transactions array
+    expenseTransactions.push(expenseTransaction);
+
+    // DYNAMICALLY CREATING AND ADDING EARNING TRANSACTION ROWS TO THE TRANSACTIONS TABLE
+    // Create the row element
+    const newExpenseRow = document.createElement('tr');
+
+    // Create the date cell
+    const expenseDateCell = document.createElement('td');
+    expenseDateCell.textContent = expenseTransaction.date;
+
+    // Create the amount cell
+    const expenseAmountCell = document.createElement('td');
+    expenseAmountCell.textContent = `$${expenseTransaction.amount}`;
+
+    // Create the description cell
+    const expenseDescriptionCell = document.createElement('td');
+    expenseDescriptionCell.textContent = expenseTransaction.description;
+
+    // Create type cell
+    const expenseTypeCell = document.createElement('td');
+    expenseTypeCell.textContent = expenseTransaction.type;
+
+    // Append each expense cell to the expense row
+    newExpenseRow.appendChild(expenseDateCell);
+    newExpenseRow.appendChild(expenseDescriptionCell);
+    newExpenseRow.appendChild(expenseAmountCell);
+    newExpenseRow.appendChild(expenseTypeCell);
+
+    // Append the completed expense row to the table body
+    tableBody.appendChild(newExpenseRow);
+
+
+
+    expenseDate.value = "";
+    expenseAmount.value = "";
+    expenseDescription.value = "";
+
+    // Dynamically creating transaction rows from expense transaction
+    closeAllModals();
 })
 
 submitBudgetBtn.addEventListener('click', function () {
@@ -169,7 +263,18 @@ submitBudgetBtn.addEventListener('click', function () {
         date: actualBudgetDate,
         amount: actualBudgetAmount,
     }
-    currentBudgetValue.textContent = Math.round((actualBudgetAmount + Number.EPSILON) * 100) / 100;
+    // Displaying the current budget value on the card summary
+    // Complicated method to round to 2 decimal places without floating point issues, but it works!
+    // currentBudgetValue.textContent = `$${Math.round(((Number(budgetTransaction.amount)) + Number.EPSILON) * 100) / 100}`;
+    fixedBudgetValue = parseFloat(budgetTransaction.amount);
+    currentBudgetValue.textContent = `$${fixedBudgetValue.toFixed(2)}`;
+
+    budgetTransactions.push(budgetTransaction);
+
+    budgetDate.value = "";
+    budgetAmount.value = "";
+
+    closeAllModals();
 })
 
 
